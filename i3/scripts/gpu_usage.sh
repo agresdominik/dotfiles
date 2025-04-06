d453460c015c057b9ea30898a7214ebd3f492341
Dominik Agres <agres.dominik@gmail.com>
2025-04-06 18:43:23 +0200

    files

---
+++ i3/scripts/gpu_usage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -f /sys/module/amdgpu/drivers/pci:amdgpu/0000:28:00.0/gpu_busy_percent ]; then
+	usage=$(awk '{print $1}' /sys/module/amdgpu/drivers/pci:amdgpu/0000:28:00.0/gpu_busy_percent)
+	echo "${usage}%"
+else
+	echo "N/A"
+fi
