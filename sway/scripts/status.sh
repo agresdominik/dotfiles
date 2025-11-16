d87753ff4eb899b6cc055e6a74ed85d1713e2d4e
Dominik Agres <dominik@agres.online>
2025-11-16 23:37:27 +0100

    sway custom bar

--- sway/scripts/status.sh
+++ sway/scripts/status.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-mem=$(free -h | awk '/Mem:/ {print $3 "/" $2}')
-cpu=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf "%.1f%%", usage}')
+mem=$(free -h --si | awk '/Mem:/ {print $3 "/" $2}')
+cpu=$(mpstat 1 1 | awk '/Average/ {print 100 - $NF"%"}')
 bat=$(cat /sys/class/power_supply/macsmc-battery/capacity 2>/dev/null || echo "N/A")
 time=$(date "+%d.%m %H:%M:%S")
+bright=$(echo "$(brightnessctl get) / 4.2" | bc)
+VPN_STATUS=$(mullvad status 2>/dev/null | grep -q '^Connected' && echo UP || echo DOWN)
+LAB_STATUS=$(ping -c1 -W1 192.168.0.200 >/dev/null 2>&1 && echo UP || echo DOWN )
 
-echo "$USER | CPU: $cpu | MEM: $mem | BAT: $bat% | $time"
+echo "$USER | LAB: $LAB_STATUS | VPN: $VPN_STATUS | CPU: $cpu | MEM: $mem | BAT: $bat% | BRI: $bright% | $time "
 
